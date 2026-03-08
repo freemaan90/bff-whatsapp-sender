a61ed15432ddb8a1726c0bda21a050cd58de85bb
freemaan90 <fredericozanoni@gmail.com>
2026-03-08 18:15:18 -0300

    se agrego una logica para borrar la carpeta whatsapp-session cada vez que se inicializa el servicio, asi obtenemos un qr nuevo asegurado;

--- src/whatsapp/whatsapp.module.ts
+++ src/whatsapp/whatsapp.module.ts
@@ -2,6 +2,8 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
 import { WhatsappController } from './whatsapp.controller';
+import { promises as fs } from 'fs';
+import * as path from 'path';
 
 @Module({
   providers: [WhatsappService],
@@ -11,6 +13,11 @@ export class WhatsappModule implements OnModuleInit {
   constructor(private readonly whatsapp: WhatsappService) {}
 
   async onModuleInit() {
+    // 1. Borrar sesión previa
+    const folderPath = path.join(process.cwd(), 'whatsapp-session');
+    await fs.rm(folderPath, { recursive: true, force: true });
+
+    // 2. Inicializar WhatsApp
     await this.whatsapp.init();
   }
 }
